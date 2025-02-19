f8e6882a7334418e69ed849561a3e71635d007e1
Natt2205 <87403060+Natt2205@users.noreply.github.com>
2025-02-19 20:40:21 +0700

    upfacebook

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/Design uten navn.png';
+import heroImg from '../../assets/natt.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
@@ -8,6 +8,8 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
+import faceLight from '../../assets/face-light.svg';
+import faceDark from '../../assets/face-dark.svg';
 import CV from '../../assets/natt-cv.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
@@ -18,6 +20,7 @@ function Hero() {
   const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const faceIcon = theme === 'light' ? faceLight : faceDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -50,6 +53,9 @@ function Hero() {
           </a>
           <a href="https://www.linkedin.com/in/nattapol-chirakitpat-64927418b/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+          <a href="https://www.facebook.com/nattapol.chirakitpat/" target="_blank">
+            <img src={faceIcon} alt="facebook icon" />
           </a>
         </span>
         <p className={styles.description}>
